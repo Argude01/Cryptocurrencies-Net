c075cb0335efe1fc25e67607b04e4c8b28ba6a66
unknown <deyaniragutierrezarteaga@gmail.com>
2021-07-20 14:41:11 -0600

    Vue.js FOR directive added with an array or collection data

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ new Vue({
     return {
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: -1
+      changePercent: 0,
+      prices: [8400, 9000, 8500, 4500, 5630, 7854, 9540],
+      
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 9000 },
+        { day: 'Miércoles', value: 8500 },
+        { day: 'Jueves', value: 4500 },
+        { day: 'Viernes', value: 5630 },
+        { day: 'Sábado', value: 7854 },
+        { day: 'Domingo', value: 9540 },
+      ]
     }
   }
 })
