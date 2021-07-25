85bd3ba04fd573cf196578d3da55fd0ce33bf0f8
unknown <deyaniragutierrezarteaga@gmail.com>
2021-07-25 12:42:38 -0600

    Directives: 1. v-on:mouseover, v-on:mouseout, v-on:click  2. v-bind:class, v-bind:src, v-bind:alt  ADDED

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ new Vue({
     return {
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 0,
+      changePercent: 1,
       //prices: [8400, 9000, 8500, 4500, 5630, 7854, 9540],
-      showPrices: false,
+      showPrices: true,
       price: 8400,
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
-        { day: 'Martes', value: 9000 },
-        { day: 'Miércoles', value: 8500 },
-        { day: 'Jueves', value: 4500 },
-        { day: 'Viernes', value: 5630 },
-        { day: 'Sábado', value: 7854 },
-        { day: 'Domingo', value: 9540 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miércoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sábado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
       ],
+      color: 'f4f4f4'
     }
   },
   // functions
   methods: {
     toggleShowPrices () {
-      this.showPrices = !this.showPrices
+      this.showPrices = !this.showPrices 
     }
+  
   }
 
 
